44baffc99e9b6cc41ba4651d4e496ae6668bd3bf
Alex Kroeger <alexrkroeger@gmail.com>
2021-11-14 15:21:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This repo contains nodejs code to analyze the COMP balances of the top accruers of COMP liquidity mining rewards.
+
+## Usage
+To run the code yourself:
+
+1. Clone the repo.
+2. Install dependencies (run `yarn install`).
+3. Create a `.env` file in the root directory by copying `.env.example` and populate the necessary environment variables (`MAINNET_URL` and `ETHERSCAN_API_KEY`).
+4. Run `npx ts-node src/index.ts` to run the script. The output data will appear will be written in the `output/` folder.
+
+Note, node and yarn are prerequisites for usage of this repo.
+
+## Input
+Data on the top 100 liquidity mining accounts has been grabbed from [Dune Analytics](https://dune.xyz) and written as a CSV to the `./input` folder.
+
+Query: https://dune.xyz/queries/231275
+
+## Analysis
+You can find some summary statistics for this data on a Google sheet [here](https://docs.google.com/spreadsheets/d/1WuazK4KYGUv2OV3IDBuyEIIVCjVcaWl1TkmoInHqWLo/edit?usp=sharing).
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+require('dotenv').config();
+
+import { ethers, Contract, BigNumber, Event } from 'ethers';
+import axios from 'axios';
+import {
+    COMP_ADDRESS,
+    CCOMP_ADDRESS,
+    CTOKEN_DELEGATE_ADDRESS,
+    GOVERNOR_BRAVO_DELEGATOR_ADDRESS,
+    GOVERNOR_BRAVO_DELEGATE_ADDRESS,
+    TARGET_BLOCK,
+} from './constants';
+import fs from 'fs';
+import parse from 'csv-parse';
+
+const TOP_ACCRURERS_PATH = `./raw_data/top_comp_accruals_at_block_${TARGET_BLOCK}.csv`;
+const OUTPUT_DATA_PATH = `./output/top_comp_accruals_with_balances_at_block_${TARGET_BLOCK}.csv`;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+async function getContractAbiFromEtherscan(contractAddress: string): Promise<string> {
+    const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${ETHERSCAN_API_KEY}`;
+    const response = await axios.get(url);
+
+    return response.data.result;
+}
+
+async function readCSVAsync(filepath: string): Promise<any[]> {
+    return new Promise<any[]>((resolve, reject) => {
+        let csvData: any[] = [];
+        fs.createReadStream(filepath)
+            .pipe(parse({delimiter: ',', from: 2}))
+            .on('data', function(row) {
+                csvData.push(row);
+            })
+            .on('end', function() {
+                resolve(csvData);  
+            });
+    });
+}
+
+function writeCSV(data: any[], filepath: string) {
+    var csv = '';
+    for (let i of data) {
+        csv += i.join(",") + "\r\n";
+    }
+
+    fs.writeFileSync(filepath, csv);
+}
+
+async function fetchGovernorBravoVoteHistory(
+    address: string,
+    governorBravoContract: Contract,
+    targetBlock: number
+): Promise<Event[]> {
+    const filter = governorBravoContract.filters.VoteCast(address);
+
+    return await governorBravoContract.queryFilter(filter,1,targetBlock);
+}
+
+async function fetchCompBalanceInfo(
+    atBlock: number,
+    account: string,
+    compContract: Contract,
+    cCompContract: Contract,
+): Promise<{compBalance: string; cCompUnderlying: string}> {
+    const compBalanceRaw: BigNumber = await compContract.balanceOf(account, { blockTag: atBlock});
+    const compBalance = ethers.utils.formatUnits(compBalanceRaw.toString(), 18);
+
+    const cCompUnderlyingRaw: BigNumber = await cCompContract.callStatic.balanceOfUnderlying(account, { blockTag: atBlock});
+    const cCompUnderlying = ethers.utils.formatUnits(cCompUnderlyingRaw.toString(), 18);
+
+    return {
+        compBalance,
+        cCompUnderlying,
+    };
+}
+
+(async () => {
+    const COMP_ABI = await getContractAbiFromEtherscan(COMP_ADDRESS);
+    const CTOKEN_ABI = await getContractAbiFromEtherscan(CTOKEN_DELEGATE_ADDRESS);
+    const GOVERNOR_BRAVO_ABI = await getContractAbiFromEtherscan(GOVERNOR_BRAVO_DELEGATE_ADDRESS);
+
+    const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_URL);
+    const compContract = new ethers.Contract(COMP_ADDRESS, COMP_ABI, provider);
+    const cCompContract = new ethers.Contract(CCOMP_ADDRESS, CTOKEN_ABI, provider);
+    const governorBravoContract = new ethers.Contract(GOVERNOR_BRAVO_DELEGATOR_ADDRESS, GOVERNOR_BRAVO_ABI, provider);
+
+    const topAccruers = await readCSVAsync(TOP_ACCRURERS_PATH);
+    
+    const outData = [];
+    outData.push(['address','comp_accrued','comp_claimed','comp_balance','comp_in_ccomp','num_votes']);
+    for (const accruer of topAccruers) {
+        const compBalances = await fetchCompBalanceInfo(TARGET_BLOCK, accruer[0], compContract, cCompContract);
+        const voteHistory = await fetchGovernorBravoVoteHistory(accruer[0], governorBravoContract, TARGET_BLOCK);
+        outData.push([accruer[0],accruer[1],accruer[2],compBalances.compBalance, compBalances.cCompUnderlying, voteHistory.length]);
+        console.log(`finished fetching on-chain data for ${accruer[0]}`);
+    }
+
+    writeCSV(outData, OUTPUT_DATA_PATH);
+})().catch(console.log);
